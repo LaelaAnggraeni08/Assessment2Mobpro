0c14209cd07f5caa7da9bf4f0679e686e81a877a
Laela Anggraeni <anggraenilaila64@gmail.com>
2023-05-18 14:28:59 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if3111/laela/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if3111.laela
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
